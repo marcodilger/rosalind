8d879e36b3f700f5ee1cf49115e845f1986ccdc4
Marco D <marcodilger@gmx.de>
2017-08-17 17:23:13 +0200

    cleaned spec.R

--- spec.R
+++ spec.R
@@ -2,9 +2,9 @@
 # 
 # The prefix spectrum of a weighted string is the collection of all its prefix weights.
 # 
-# Given: A list LL of nn (n≤100n≤100) positive real numbers.
+# Given: A list L  of n (n<=100) positive real numbers.
 # 
-# Return: A protein string of length n−1n−1 whose prefix spectrum is equal to LL (if multiple solutions exist, you may output any one of them). Consult the monoisotopic mass table.
+# Return: A protein string of length n-1 whose prefix spectrum is equal to L (if multiple solutions exist, you may output any one of them). Consult the monoisotopic mass table.
 # 
 # Sample Dataset
 # 
@@ -20,12 +20,10 @@
 # 
 #
 
-
 start <- Sys.time()
 mass_table <- read.table("data/monoisotopic_mass_table.txt", stringsAsFactors = FALSE)
 
 replicate(100, {
-
 mass_table[ ,2] <- round(as.numeric(mass_table[ ,2]), 4)
 
 data <- as.numeric(readLines("data/rosalind_spec.txt"))
@@ -33,29 +31,6 @@ mass <- data[-1] - data[-length(data)]
 
 peptide <- mass_table[match(round(mass,4), round(mass_table[ ,2],4)) ,1]
 writeClipboard(paste0(peptide, collapse=""))
-})
-
-print(Sys.time()-start)
-
-
-
-
-
-# fremde lösungen
-#
-start <- Sys.time()
-masses <- read.table("data/monoisotopic_mass_table2.txt", row.names = 1, header = TRUE);
-
-
-replicate(100, {
-  
-  
-  input <- as.numeric(readLines("data/rosalind_spec.txt"));
-  diffs <- matrix(diff(input),1,length(input)-1);
-  
-  delta <- apply(diffs,2,function(x){abs(log(x / masses$Mass))});
-  cat(rownames(masses)[apply(delta,2,function(x){which(x == min(x))[1]})],
-      "\n",sep="");
 })
 
 print(Sys.time()-start)
